8b8d748fa7ca67170fbd7b480f090cb17b398b24
evansdr1 <evansdr@oregonstate.edu>
2017-06-12 16:34:07 -0700

    Added in some of the JS for the page

--- public/index.js
+++ public/index.js
@@ -1,1 +1,20 @@
+function randomize(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}   //Code Obtained on mozilla developer website
 
+function randomButton(type, stat){
+	var statField = stat.getElementsByClassName('stat');
+	statField.createTextNode(randomize(0, type));
+}
+
+function incrementStat(stat){
+	var statFieldValue = stat.getElementsByClassName('stat').textContent;
+	statFieldValue+=1;
+}
+
+function decrementStat(stat){
+	var statFieldValue = stat.getElementsByClassName('stat').textContent;
+	statFieldValue-=1;
+}
