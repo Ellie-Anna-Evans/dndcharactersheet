2aa4412652ca1b1a8098ae3a0dde229f687a86be
evansdr1 <evansdr@oregonstate.edu>
2017-06-12 11:59:34 -0700

    Added framework to server.js

--- server.js
+++ server.js
@@ -9,6 +9,25 @@ var port = process.env.PORT || 3000;
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.get('/', function(req, res, next){
+	
+});
+
+app.get('/chsheet/:index', function(req, res, next){
+	
+});
+
+app.get('/style.css', function(req, res, next){
+	res.status(200).sendFile(path.join(__dirname, 'public', 'style.css'));
+});
+
+app.get('/index.js', function(req, res, next){
+	res.status(200).sendFile(path.join(__dirname, 'public', 'index.js'));
+});
+
+app.get('*', function(req, res, next){
+	res.status(404).render('404Page');
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
